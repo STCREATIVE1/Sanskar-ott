bc4e0dcba1b8c8aacd6cefc677627078ff30b77f
STCREATIVE1 <sanskarneet202@gmail.com>
2026-01-17 02:35:50 +0530

    Integrate Sony events fetching into fetcher.php
    Added a function to fetch Sony events and merged with existing Zee channels.

--- api/fetcher.php
+++ api/fetcher.php
@@ -1,9 +1,37 @@
 <?php
+// api/fetcher.php
 header('Content-Type: application/json');
-// Ye script future mein links update karne ke kaam aayegi
-$channels = [
+
+function getSonyEvents() {
+    $url = "https://msapi.sonyliv.com/privileged/v1/content/live/events?contentId=sports";
+    $ch = curl_init($url);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_USERAGENT, "Mozilla/5.0 (Windows NT 10.0; Win64; x64)");
+    $res = curl_exec($ch);
+    $data = json_decode($res, true);
+    
+    $events = [];
+    if(isset($data['resultObj']['containers'][0]['assets'])) {
+        foreach($data['resultObj']['containers'][0]['assets'] as $asset) {
+            $events[] = [
+                "title" => $asset['title'],
+                "id" => $asset['contentId'],
+                "type" => "sony",
+                "img" => $asset['image_url']
+            ];
+        }
+    }
+    return $events;
+}
+
+$zee_channels = [
     ["title" => "Zee Cinema HD", "id" => "0-9-zeecinemahd", "type" => "zee5", "img" => "https://static.zee5.com/images/ZEE_CINEMA_HD.png"],
-    ["title" => "Sony Sports 1", "id" => "sony_sports_1", "type" => "sony", "img" => "https://upload.wikimedia.org/wikipedia/en/2/23/Sony_LIV_logo.png"]
+    ["title" => "Zee TV HD", "id" => "0-9-zeetvhd", "type" => "zee5", "img" => "https://static.zee5.com/images/ZEE_TV_HD.png"]
 ];
-echo json_encode($channels);
+
+$sony_events = getSonyEvents();
+$final_data = array_merge($zee_channels, $sony_events);
+
+// Ye output GitHub Action capture karega
+echo json_encode($final_data, JSON_PRETTY_PRINT);
 ?>
