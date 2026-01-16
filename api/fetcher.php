71b1815cf2bd6db998a3c4ca7a283a2a940f1d53
STCREATIVE1 <sanskarneet202@gmail.com>
2026-01-17 03:05:56 +0530

    Implement Sony events fetching and merging logic
    Added a function to fetch Sony events and merge them with Zee channels. Updated the final output to include Sony events if available.

--- api/fetcher.php
+++ api/fetcher.php
@@ -1,10 +1,41 @@
 <?php
-// api/fetcher.php ke start mein ye zaroor rakhein
-error_reporting(0); 
+error_reporting(0);
 header('Content-Type: application/json');
 
-// ... baaki ka logic ...
+function getSonyEvents() {
+    $url = "https://msapi.sonyliv.com/privileged/v1/content/live/events?contentId=sports";
+    $ch = curl_init($url);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_USERAGENT, "Mozilla/5.0 (Windows NT 10.0; Win64; x64)");
+    curl_setopt($ch, CURLOPT_TIMEOUT, 10);
+    $res = curl_exec($ch);
+    curl_close($ch);
+    
+    $data = json_decode($res, true);
+    $events = [];
+    
+    if(isset($data['resultObj']['containers'][0]['assets'])) {
+        foreach($data['resultObj']['containers'][0]['assets'] as $asset) {
+            $events[] = [
+                "title" => $asset['title'],
+                "id" => $asset['contentId'],
+                "type" => "sony",
+                "img" => $asset['image_url']
+            ];
+        }
+    }
+    return $events;
+}
 
-// Last mein sirf ye print karein
-echo json_encode($final_data);
+$zee_channels = [
+    ["title" => "Zee Cinema HD", "id" => "0-9-zeecinemahd", "type" => "zee5", "img" => "https://static.zee5.com/images/ZEE_CINEMA_HD.png"],
+    ["title" => "Zee TV HD", "id" => "0-9-zeetvhd", "type" => "zee5", "img" => "https://static.zee5.com/images/ZEE_TV_HD.png"]
+];
+
+$sony_events = getSonyEvents();
+
+// Agar Sony ka data nahi mila toh sirf Zee channels dikhayega
+$final_data = (!empty($sony_events)) ? array_merge($zee_channels, $sony_events) : $zee_channels;
+
+echo json_encode($final_data, JSON_UNESCAPED_SLASHES | JSON_PRETTY_PRINT);
 ?>
