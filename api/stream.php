628308f6e5e636dbfef88e9f0d58c05cecb066da
STCREATIVE1 <sanskarneet202@gmail.com>
2026-01-17 02:45:21 +0530

    Update stream.php

--- api/stream.php
+++ api/stream.php
@@ -1,15 +1,28 @@
 <?php
+// api/stream.php
 $id = $_GET['id'] ?? '';
 $type = $_GET['type'] ?? '';
 
+if (empty($id)) {
+    die("No ID provided");
+}
+
 if ($type == 'zee5') {
+    // Zee5 Logic
     $api = "https://catalogapi.zee5.com/v1/channel/" . $id;
     $token_api = "https://useraction.zee5.com/token/live.php";
     $res = json_decode(file_get_contents($api));
     $tok = json_decode(file_get_contents($token_api));
     header("Location: " . $res->stream_url_hls . $tok->video_token);
-} else {
-    // Sony ya anya ke liye fallback link
-    header("Location: https://dai.google.com/linear/hls/event/sid/master.m3u8");
+    exit;
+
+} elseif ($type == 'sony') {
+    // SonyLIV Live Events Logic
+    // Hum ek working server ka use karenge jo SonyLIV ke tokens handle karta hai
+    $sony_url = "https://sony-api-seven.vercel.app/get_m3u8?id=" . $id; 
+    
+    // Redirecting to the live stream
+    header("Location: " . $sony_url);
+    exit;
 }
 ?>
