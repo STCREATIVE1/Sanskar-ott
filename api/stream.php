e786778706a3937e511ffc329ff1ee7ce6c848ce
STCREATIVE1 <sanskarneet202@gmail.com>
2026-01-17 13:23:27 +0530

    Update stream.php

--- api/stream.php
+++ api/stream.php
@@ -1,29 +1,29 @@
 <?php
-// api/stream.php
+// Sabse upar CORS headers taaki player block na kare
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/vnd.apple.mpegurl");
+
 $id = $_GET['id'] ?? '';
 $type = $_GET['type'] ?? '';
 
 if ($type == 'zee5') {
     $api = "https://catalogapi.zee5.com/v1/channel/" . $id;
     $token_api = "https://useraction.zee5.com/token/live.php";
+    
     $res = json_decode(@file_get_contents($api));
     $tok = json_decode(@file_get_contents($token_api));
+    
     if($res && isset($res->stream_url_hls)) {
-        header("Location: " . $res->stream_url_hls . $tok->video_token);
+        $finalUrl = $res->stream_url_hls . $tok->video_token;
+        // Direct Redirect to m3u8
+        header("Location: $finalUrl");
         exit;
     }
 } 
 
 if ($type == 'sony') {
-    // Sony channels ke liye hum public stream source use karenge
-    // Ten 1 aur Ten 5 ke liye temporary working links
-    $sony_links = [
-        "sony_ten1" => "https://pubads.g.doubleclick.net/ssai/event/uV3R_Xm_T66vXzXz5XzXzX/master.m3u8",
-        "sony_ten5" => "https://pubads.g.doubleclick.net/ssai/event/XiS_Xm_T66vXzXz5XzXzX/master.m3u8"
-    ];
-    
-    $final_url = $sony_links[$id] ?? "https://dai.google.com/linear/hls/event/sid/master.m3u8";
-    header("Location: " . $final_url);
+    $sony_resolver = "https://sony-api-seven.vercel.app/get_m3u8?id=" . $id; 
+    header("Location: $sony_resolver");
     exit;
 }
 ?>
